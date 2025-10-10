535bad4b9a6bfe681c6893e48f01b27fc3231c6f
denyschr <denprogman99@gmail.com>
2025-10-10 21:42:48 +0300

    word frequency counter

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,13 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  return str
+    .toLowerCase()
+    .replace(/[^\w\s]|_/g, '')
+    .split(' ')
+    .reduce(
+      (map, word) =>
+        word !== '' ? map.set(word, (map.get(word) ?? 0) + 1) : map,
+      new Map()
+    );
+}
 
 module.exports = wordFrequencyCounter;
