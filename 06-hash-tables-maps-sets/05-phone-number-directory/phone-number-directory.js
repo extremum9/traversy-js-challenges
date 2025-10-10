555a4cc713ba6deb08275db65c80bf96c9e7ee57
denyschr <denprogman99@gmail.com>
2025-10-10 21:52:25 +0300

    phone number directory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,8 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneNumbers) {
+  return phoneNumbers.reduce((map, phoneNumber) => {
+    const formatted = phoneNumber.split(':');
+    return map.set(formatted[0], formatted[1]);
+  }, new Map());
+}
 
 module.exports = phoneNumberDirectory;
