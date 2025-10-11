44974ff39e252499f8768ba5846d09a5e2354cc2
denyschr <denprogman99@gmail.com>
2025-10-11 11:15:02 +0300

    symmetric difference

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,11 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const difference1 = [...set1].filter((num) => !set2.has(num));
+  const difference2 = [...set2].filter((num) => !set1.has(num));
+
+  return [...difference1, ...difference2];
+}
 
 module.exports = symmetricDifference;
