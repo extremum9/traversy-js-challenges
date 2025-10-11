6df371a592b2ef592f28ed45193d1fea3de1e8bd
denyschr <denprogman99@gmail.com>
2025-10-11 09:58:52 +0300

    anagram grouping

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,21 @@
-function anagramGrouping() {}
+function getHash(word) {
+  return [...word].sort().join('');
+}
+
+function anagramGrouping(words) {
+  return [
+    ...words
+      .reduce((map, word) => {
+        const hash = getHash(word);
+        if (map.has(hash)) {
+          map.get(hash).push(word);
+        } else {
+          map.set(hash, [word]);
+        }
+        return map;
+      }, new Map())
+      .values()
+  ];
+}
 
 module.exports = anagramGrouping;
