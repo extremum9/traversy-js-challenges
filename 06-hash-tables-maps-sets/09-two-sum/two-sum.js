b1cabd2b86d4558c14d1291f342a0923265489f8
extremum9 <denprogman99@gmail.com>
2025-10-22 18:42:37 +0300

    two sum

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,14 @@
-function twoSum() {}
+function twoSum(nums, target) {
+  const lib = new Map();
+  for (let index = 0; index < nums.length; index++) {
+    const num = nums[index];
+    const remainder = target - num;
+    if (lib.has(remainder)) {
+      return [lib.get(remainder), index];
+    }
+    lib.set(num, index);
+  }
+  return [];
+}
 
 module.exports = twoSum;
