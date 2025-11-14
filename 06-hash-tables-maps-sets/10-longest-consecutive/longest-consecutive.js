a0609674104b72dda5dbb2229f07b9e7f8348948
extremum9 <denprogman99@gmail.com>
2025-11-14 22:05:22 +0200

    longest consecutive

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,22 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+  const set = new Set(nums);
+  let longest = 0;
+
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentSequence = 1;
+
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentSequence++;
+      }
+
+      longest = Math.max(longest, currentSequence);
+    }
+  }
+
+  return longest;
+}
 
 module.exports = longestConsecutiveSequence;
