4cb69e582b69e554c90a2694ebb3289ec7188747
extremum9 <denprogman99@gmail.com>
2025-11-22 01:23:34 +0200

    custom hash table part 1

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,9 @@
 const HashTable = require('./custom-hash-table');
+
+const hashTable = new HashTable();
+
+hashTable.set('John', '555-234-3455');
+hashTable.set('James', '555-349-3948');
+hashTable.set('Sara', '555-469-3334');
+
+hashTable.print();
